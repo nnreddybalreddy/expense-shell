6c776cf2795782e4b0c9b4e04ea932a8f2f92e02
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-16 13:47:18 -0500

    ALL

--- frontend.sh
+++ frontend.sh
@@ -52,6 +52,9 @@ VALIDATE $? "start nginx"
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "remove html"
 
+rm -rf /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "remove zip for temp"
+
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "code for tmp"
 
